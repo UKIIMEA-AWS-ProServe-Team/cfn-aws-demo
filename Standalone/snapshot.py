7d0eef06aa8927292842cb0321382878ff8a1a4e
ianp5uk <ianp5uk@outlook.com>
2020-04-18 14:23:52 +0100

    Work on snapshot

--- Standalone/snapshot.py
+++ Standalone/snapshot.py
@@ -3,39 +3,51 @@ import collections
 import datetime
  
 ec = boto3.client('ec2')
- 
+
 def lambda_handler(event, context):
+
     reservations = ec.describe_instances(
         Filters=[
-            {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
+            {'Name': 'tag-key', 'Values': ['Backup1357']},
         ]
     ).get(
         'Reservations', []
     )
- 
+
     instances = sum(
         [
             [i for i in r['Instances']]
             for r in reservations
         ], [])
- 
-    print "Found %d instances that need backing up" % len(instances)
- 
+    if len(instances) > 1:
+        print ("ERROR: found more than 1 instance to backup")
+        return None
+
     for instance in instances:
         for dev in instance['BlockDeviceMappings']:
             if dev.get('Ebs', None) is None:
                 continue
             vol_id = dev['Ebs']['VolumeId']
-            print "Found EBS volume %s on instance %s" % (
-                vol_id, instance['InstanceId'])
- 
+            print "Found EBS volume %s on instance %s" % (vol_id, instance['InstanceId'])
+
             snap = ec.create_snapshot(
+                Description='Jenkins snapshot',
                 VolumeId=vol_id,
             )
- 
-             print "Retaining snapshot %s of volume %s from instance %s" % (
+
+            ec.create_tags(
+                Resources=[
+                    snap['SnapshotId'],
+                ],
+                Tags=[
+                    {'Key': 'Name', 'Value': "Jenkins snap"},
+                    {'Key': 'Date', 'Value': "UTC: %s" % (str(datetime.datetime.utcnow()))}
+                ]
+            )            
+
+            print "Snapshot %s of volume %s from instance %s" % (
                 snap['SnapshotId'],
                 vol_id,
                 instance['InstanceId'],
             )
-       )
+lambda_handler(None, None)
