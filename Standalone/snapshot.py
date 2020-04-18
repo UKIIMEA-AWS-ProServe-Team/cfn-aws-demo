d19920fb86cc31cf6aa4d243c6484321fe0829a8
ianp5uk <ianp5uk@outlook.com>
2020-04-18 16:41:38 +0100

    snap work

--- Standalone/snapshot.py
+++ Standalone/snapshot.py
@@ -1,8 +1,8 @@
 import boto3
-import collections
 import datetime
  
 ec = boto3.client('ec2')
+store = boto3.client('ssm')
 
 def lambda_handler(event, context):
 
@@ -19,35 +19,46 @@ def lambda_handler(event, context):
             [i for i in r['Instances']]
             for r in reservations
         ], [])
-    if len(instances) > 1:
-        print ("ERROR: found more than 1 instance to backup")
+    if len(instances) != 1:
+        print ("ERROR: found more than 1 instance to backup & that is not the intent")
         return None
+    else:
+        for instance in instances:
+            for dev in instance['BlockDeviceMappings']:
+                if dev.get('Ebs', None) is None:
+                    continue
+                vol_id = dev['Ebs']['VolumeId']
+                print "Found EBS volume %s on instance %s" % (vol_id, instance['InstanceId'])
 
-    for instance in instances:
-        for dev in instance['BlockDeviceMappings']:
-            if dev.get('Ebs', None) is None:
-                continue
-            vol_id = dev['Ebs']['VolumeId']
-            print "Found EBS volume %s on instance %s" % (vol_id, instance['InstanceId'])
+                snap = ec.create_snapshot(
+                    Description='Jenkins snapshot',
+                    VolumeId=vol_id,
+                )
 
-            snap = ec.create_snapshot(
-                Description='Jenkins snapshot',
-                VolumeId=vol_id,
-            )
 
-            ec.create_tags(
-                Resources=[
+                savesnap = client.put_parameter(
+                    Name='/JenkinsSnapshotId',
+                    Value=snap['SnapshotId'],
+                    Type='String',
+                    KeyId='string',
+                    Overwrite=True,
+                    Tier='Standard',
+                )                    
+
+                ec.create_tags(
+                    Resources=[
+                        snap['SnapshotId'],
+                    ],
+                    Tags=[
+                        {'Key': 'Name', 'Value': "Jenkins snap"},
+                        {'Key': 'Date', 'Value': "UTC: %s" % (str(datetime.datetime.utcnow()))}
+                    ]
+                )            
+
+                print "Snapshot %s of volume %s from instance %s" % (
                     snap['SnapshotId'],
-                ],
-                Tags=[
-                    {'Key': 'Name', 'Value': "Jenkins snap"},
-                    {'Key': 'Date', 'Value': "UTC: %s" % (str(datetime.datetime.utcnow()))}
-                ]
-            )            
+                    vol_id,
+                    instance['InstanceId'],
+                )
 
-            print "Snapshot %s of volume %s from instance %s" % (
-                snap['SnapshotId'],
-                vol_id,
-                instance['InstanceId'],
-            )
 lambda_handler(None, None)
