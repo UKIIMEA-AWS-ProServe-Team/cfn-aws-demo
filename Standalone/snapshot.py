3d07e794cc9e0e63764c3452bfe6115db72775f1
ianp5uk <ianp5uk@outlook.com>
2020-04-19 18:11:08 +0100

    WIP

--- Standalone/snapshot.py
+++ Standalone/snapshot.py
@@ -1,3 +1,4 @@
+# Need to deal with terminated instances that have the tag
 import boto3
 import datetime
  
@@ -8,7 +9,7 @@ def lambda_handler(event, context):
 
     reservations = ec.describe_instances(
         Filters=[
-            {'Name': 'tag-key', 'Values': ['Backup1357']},
+            {'Name': 'tag-key', 'Values': ['Backup1357']}
         ]
     ).get(
         'Reservations', []
@@ -19,53 +20,55 @@ def lambda_handler(event, context):
             [i for i in r['Instances']]
             for r in reservations
         ], [])
-    if len(instances) != 1:
-        print ("ERROR: found more than 1 instance to backup & that is not the intent")
-        return None
-    else:
-        # If everything looks ok proceed
-        for instance in instances:
-            print "Found instance %s" % (instance['InstanceId'])
 
-            # Create an AMI from the instance
-            JenkinsAMI = client.create_image(
-                BlockDeviceMappings=[
-                    {
-                        'DeviceName': '/dev/xvda',
-                        'Ebs': {
-                            'DeleteOnTermination': True
-                         }
-                        },
-                ],
-                Description='Jenkins AMI from instance',
-                InstanceId=instance['InstanceId'],
-                Name='Jenkins AMI'
-            )                      
+    count = 0
+    for instance in instances:
+        if instance['State']['Name'] != 'running':      # don't want to backup non-running instances left after rapid restart of stack
+            continue
+        count += 1
+        if count > 1:
+            print("Oops more than 1 instance - something's wrong - aborting!")
+            return
+        print "Found instance %s" % (instance['InstanceId'])
+        # Create an AMI from the instance
+        JenkinsAMI = ec.create_image(
+            BlockDeviceMappings=[
+                {
+                    'DeviceName': '/dev/xvda',
+                    'Ebs': {
+                        'DeleteOnTermination': True
+                        }
+                    },
+            ],
+            Description='Jenkins AMI from instance',
+            InstanceId=instance['InstanceId'],
+            # Need unique name here hence addition of datetime
+            Name='JenkinsAMI ' + str(datetime.datetime.utcnow()).replace(':' , '-') 
+        )                      
 
-            # Tag the AMI
-            ec.create_tags(
-                Resources=[
-                    JenkinsAMI['ImageId']
-                ],
-                Tags=[
-                    {'Key': 'Name', 'Value': "Jenkins AMI"},
-                    {'Key': 'Date', 'Value': "UTC: %s" % (str(datetime.datetime.utcnow()))}
-                ]
-            )
+        # Tag the AMI
+        ec.create_tags(
+            Resources=[
+                JenkinsAMI['ImageId']
+            ],
+            Tags=[
+                {'Key': 'Name', 'Value': "Jenkins AMI"},
+                {'Key': 'Date', 'Value': "UTC: %s" % (str(datetime.datetime.utcnow()))}
+            ]
+        )
 
-            print "AMI %s created from instance %s" % (
-                JenkinsAMI['ImageId'],
-                instance['InstanceId']
-            )
+        print "AMI %s created from instance %s" % (
+            JenkinsAMI['ImageId'],
+            instance['InstanceId']
+        )
 
-            # Save the AMI id to Parameter store for retrieval by server boot
-            saveami = client.put_parameter(
-                Name='/JenkinsAMIId',
-                Value=JenkinsAMI['ImageId'],
-                Type='String',
-                Overwrite=True,
-                Tier='Standard',
-            )           
-
+        # Save the AMI id to Parameter store for retrieval by server boot
+        saveami = store.put_parameter(
+            Name='/JenkinsAMIId',
+            Value=JenkinsAMI['ImageId'],
+            Type='String',
+            Overwrite=True,
+            Tier='Standard',
+        )
 
 lambda_handler(None, None)
